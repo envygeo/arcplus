eabc4b2b0b9029b166c599de98624be0f8660d5f
matt wilkie <maphew@gmail.com>
2013-11-13 10:28:34 -0800

    cleaner product-codes.txt parsing

--- SysAdmin/product-codes.bat
+++ SysAdmin/product-codes.bat
@@ -1,18 +1,19 @@
 @echo off
-:: [28709 - Silently uninstall ArcGIS products](http://support.esri.com/en/knowledgebase/techarticles/detail/28709)
+:: [Silently uninstall ArcGIS products](http://support.esri.com/en/knowledgebase/techarticles/detail/28709)
 ::
 :: The list above is not complete, it's missing ArcPad and maybe others.
-:: Product Codes for most Esri setups can be found in the setup.ini file delivered with the other installation files.
+:: Product Codes for most Esri setups can be found in the setup.ini file delivered with 
+:: the other installation files.
 ::
-:: Syntax of "product-codes.txt", any line without a left curly brace "{" is ignored
+:: Syntax of "product-codes.txt":
 ::
-::  Product Name {a1a1a1a-a1a1a-a1a1a...}
+::    Product Name {a1a1a1a-a1a1a-a1a1a...}
 ::
-::  ArcGIS 8.2
-::  ArcGIS Desktop {A149DEA2-1D5B-11D5-9F76-00C04F6BC7A1}
-::  ArcGIS ArcObjects Developer Kit {52069752-B5E9-11D5-8110-00C04FA070E5}
-::  ArcGIS Tutorial Data {440A069B-9016-11D4-80CB-00C04FA070E5}
-::  ...etc.
+::    ArcGIS 8.2
+::    ArcGIS Desktop {A149DEA2-1D5B-11D5-9F76-00C04F6BC7A1}
+::    ArcGIS ArcObjects Developer Kit {52069752-B5E9-11D5-8110-00C04FA070E5}
+::    ArcGIS Tutorial Data {440A069B-9016-11D4-80CB-00C04FA070E5}
+::    ...etc.
 
 call :read_file
 goto :eof
@@ -21,8 +22,8 @@ goto :eof
   :: Parse product name and id code from Product-Codes.txt
   :: http://ss64.com/nt/for_f.html
   for /f "tokens=1,2 delims=^{" %%a in (product-codes.txt) do (^
-    echo Product: %%a
-    echo code:  {%%b
+    rem Ignore any line without a left curly brace
+    if not "%%b"=="" echo {%%b - %%a
     )
   goto :eof
 
