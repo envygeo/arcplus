1237f7e82e9a90bd7868cc6f6be384c8840de2fd
matt wilkie <maphew@gmail.com>
2013-11-13 10:55:58 -0800

    update uninstall all to use product text file

--- SysAdmin/uninstall-ALL-ArcGIS-products.bat
+++ SysAdmin/uninstall-ALL-ArcGIS-products.bat
@@ -14,14 +14,15 @@ if /i not "%_verify%"=="Y" goto :eof
 set _opt=/passive 
 if "%2"=="silent" set _opt=
 
-call :v8.2 %_opt%
-call :v8.3 %_opt%
-call :v9.0 %_opt%
-call :v9.1 %_opt%
-call :v9.2 %_opt%
-call :v9.3x %_opt%
-call :v10 %_opt%
-call :v10.1 %_opt%
+rem call :v8.2 %_opt%
+rem call :v8.3 %_opt%
+rem call :v9.0 %_opt%
+rem call :v9.1 %_opt%
+rem call :v9.2 %_opt%
+rem call :v9.3x %_opt%
+rem call :v10 %_opt%
+rem call :v10.1 %_opt%
+call :from_file test-product-codes.txt
 
 echo.
 echo. Finished, log at:
@@ -52,11 +53,38 @@ goto :eof
 :: Product codes are from 
 ::  http://support.esri.com/en/knowledgebase/techarticles/detail/28709
 ::
-:: They can also be retrieved dynamically with (be patient, takes a long time):
+:: The list above is not complete, it's missing ArcPad and maybe others.
+:: Product Codes for most Esri setups can be found in the setup.ini file delivered with 
+:: the other installation files.
+::
+:: Syntax of "product-codes.txt":
+::
+::    Product Name {a1a1a1a-a1a1a-a1a1a...}
+::
+::    ArcGIS 8.2
+::    ArcGIS Desktop {A149DEA2-1D5B-11D5-9F76-00C04F6BC7A1}
+::    ArcGIS ArcObjects Developer Kit {52069752-B5E9-11D5-8110-00C04FA070E5}
+::    ArcGIS Tutorial Data {440A069B-9016-11D4-80CB-00C04FA070E5}
+::    ...etc.
+::
+:: -----
+:: An alternate approach is to retrieve dynamically with (be patient, takes a long time):
 ::
 ::  wmic product where "Name like '%ArcGIS%'" ^
 ::    get Name, IdentifyingNumber, Version 
 ::
+
+:from_file
+  :: Parse product name and id code from Product-Codes.txt
+  :: http://ss64.com/nt/for_f.html
+  echo.
+  for /f "tokens=1,2 delims=^{" %%a in (%1) do (^
+    if "%%b"=="" (echo ----- %%a) else (echo {%%b - %%a)
+    call :uninstall {%%b
+    )
+  goto :eof
+
+
 :v8.2
   echo. %~0
   for %%a in (^
