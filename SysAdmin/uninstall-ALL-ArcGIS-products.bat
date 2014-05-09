e07ad6679655605e12335e6e227888ddcad33286
Matt Wilkie <maphew@gmail.com>
2014-05-09 13:06:52 -0700

    remove excessive verify prompt; make "/silent" actually work; move code reporting next to function which actually does the work

--- SysAdmin/uninstall-ALL-ArcGIS-products.bat
+++ SysAdmin/uninstall-ALL-ArcGIS-products.bat
@@ -5,20 +5,14 @@
 ::
 set _product_codes=%1
 echo. ---------------------------------------------------------------------------
-echo.
 echo.           Uninstall all ArcGIS products
-echo.
-echo.           using "%_product_codes%" for Product ID list
 echo. ---------------------------------------------------------------------------
-echo.
 if not exist "%1" goto :no_codes_file
-set /p _verify="Are you sure you want to continue? [y/N] " || set _verify=N
-if /i not "%_verify%"=="Y" goto :eof
 
 :: msiexec optional parameters here
 :: remove '/passive' to hide the pop-up progress window and run silently
 set _opt=/passive 
-if "%2"=="silent" set _opt=
+if "%2"=="/silent" set _opt=
 
 call :from_file %_product_codes%
 
@@ -41,6 +35,8 @@ goto :eof
   :: Parse product name and id code from Product-Codes.txt
   :: http://ss64.com/nt/for_f.html
   echo.
+  echo.           using "%_product_codes%" for Product ID list
+ 
   for /f "tokens=1,2 delims=^{" %%a in (%1) do (^
     if "%%b"=="" (echo ----- %%a -----) else (echo {%%b - %%a)
     call :uninstall {%%b
@@ -49,9 +45,10 @@ goto :eof
 
 :no_codes_file
   echo.
-  echo. "%1" not found
+  echo. Product code file "%1" not found
   echo.
   call :Usage
+  echo.&& pause
   goto :eof
 
 :Usage  
