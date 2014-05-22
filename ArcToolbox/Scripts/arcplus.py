23add78f091d10589aaf96c2378f0ab1fa58f825
Matt Wilkie <maphew@gmail.com>
2014-05-22 10:59:16 -0700

    arcplus.listAllFeatureClasses: upgrade for 10.2

--- ArcToolbox/Scripts/arcplus.py
+++ ArcToolbox/Scripts/arcplus.py
@@ -1,16 +1,17 @@
 #@+leo-ver=5-thin
-#@+node:maphew.20120201135209.1859: * @file arcplus.py
+#@+node:maphew.20140522093157.1506: * @file arcplus.py
 ''' arcplus.py: functions missing from regular ol' arcpy module '''
 
-import os, arcgisscripting
-gp = arcgisscripting.create(9.3)
+import os
+import arcpy
+    
 
 #@+others
-#@+node:maphew.20110208134351.3123: ** list All Feature Classes
+#@+node:maphew.20140522093157.1509: ** listAllFeatureClasses
 def listAllFeatureClasses (gdb,**kwargs):
     ''' list all Feature Classes in a geodatabase or coverage recursively (normal listFeatureClasses does not recurse)'''
 
-    gp.workspace = gdb
+    arcpy.env.workspace = gdb
 
     if not kwargs.has_key('fc_filter'): fc_filter = '*'
     else: fc_filter = kwargs ['fc_filter']
@@ -18,16 +19,15 @@ def listAllFeatureClasses (gdb,**kwargs):
     if not kwargs.has_key('fd_filter'): fd_filter = '*'
     else: fd_filter = kwargs ['fd_filter']
 
-    print 'Looking in %s for "%s" ' % (gp.workspace,fc_filter)
+    print 'Looking in %s for "%s" ' % (arcpy.env.workspace,fc_filter)
 
     fcs = []
-    for fds in gp.ListDatasets(fd_filter,'feature') + ['']:
-        for fc in gp.ListFeatureClasses(fc_filter,'',fds):
+    for fds in arcpy.ListDatasets(fd_filter,'feature') + ['']:
+        for fc in arcpy.ListFeatureClasses(fc_filter,'',fds):
             #print '%s\\%s' % (fds,fc)
             fcs.append(os.path.join(fds,fc))
 
     return fcs
-
 #@+node:maphew.20110314104726.3195: *3* extended docstring
 '''
 Optional keyword arguments, for example:
