ed9f83aa167f3e2d458dfc58cf7d26e539831d36
Matt.Wilkie <Matt.Wilkie@gov.yk.ca>
2014-12-03 13:32:42 -0800

    revert to what works, tables only

--- ArcToolbox/Scripts/export_gdb_domains.py
+++ ArcToolbox/Scripts/export_gdb_domains.py
@@ -20,18 +20,6 @@ def export_domains(domains):
         arcpy.DomainToTable_management(gdb, domain.name, table,
             'field','description', '#')
 
-def export_domain_meta(domains):
-    table = 'domain_metadata'
-    arcpy.AddMessage('Saving some domain metadata to %s' % table)
-    fields = 'Name, Description, Type'.split(',')
-    arcpy.CreateTable_management(gdb, table)
-    for f in fields:
-        arcpy.AddField_management(table, f, "TEXT")
-    cursor = arcpy.da.InsertCursor(table, fields)
-    for d in domains:
-        cursor.InsertRow(d.name, d.description, d.type)
-
 if __name__ == "__main__":
     domains = arcpy.da.ListDomains(gdb)
     export_domains(domains)
-    export_domain_meta(domains)
