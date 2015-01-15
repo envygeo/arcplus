38e3a228a2744e64a9afdea43b5b4368ea77f2b0
Matt.Wilkie <Matt.Wilkie@gov.yk.ca>
2015-01-15 14:38:13 -0800

    make version agnostic, add error checking
    I'm not sure AGSDESKTOPJAVA is most reliable method of determining
    install location. We might need to read registry instead.

--- SysAdmin/ArcMap-Advanced.bat
+++ SysAdmin/ArcMap-Advanced.bat
@@ -1,4 +1,37 @@
-set _fname=%~n0
-set _level=%_fname:*-=%
-set ESRI_SOFTWARE_CLASS=%_level%
-start /b c:\ArcGIS\Desktop10.2\bin\ArcMap.exe
+@echo off
+if not defined AGSDESKTOPJAVA goto :NoEnv
+set _exe="%AGSDESKTOPJAVA%\bin\ArcMap.exe"
+if not exist %_exe% goto :NoExe
+
+call :GetLicense
+call :Main
+goto :eof
+
+:: ----------------------------------------------
+:GetLicense
+    :: Determine License level from filename suffix
+    :: ArcMap-Editor.bat --> "Editor" license
+    set _fname=%~n0
+    set _level=%_fname:*-=%
+    set ESRI_SOFTWARE_CLASS=%_level%
+    goto :eof
+    
+:Main
+    start "Starting ArcMap %_level%" /b %_exe%
+    goto :eof
+
+:NoEnv
+    echo.
+    echo.   AGSDESKTOPJAVA is not defined.
+    echo.   I can't determine where ArcGIS is installed.
+    echo.
+    pause
+    goto :eof
+
+:NoExe
+    echo.
+    echo.   *** Error: %_exe% not found.
+    echo.
+    pause
+    goto :eof
+    
