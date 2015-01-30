536e70e116e951560f4bb95492fb19369b6726a0
Matt.Wilkie <Matt.Wilkie@gov.yk.ca>
2015-01-30 11:20:19 -0800

    local ssd test

--- arcplus/profile-arcpy-listFC.py
+++ arcplus/profile-arcpy-listFC.py
@@ -1,12 +1,14 @@
 import arcpy
 
-gdb = r'X:\Env-dat.081\source\yt_courbe_niveau_imperial.gdb'
+#gdb = r'X:\Env-dat.081\source\yt_courbe_niveau_imperial.gdb'
+gdb = r'D:\s\yt_courbe_niveau_imperial.gdb'
 
 
 def arcpy_listFC(gdb):
     arcpy.env.workspace = gdb
-    print 'Looking in "%s" ' % (arcpy.env.workspace)
+    print 'Looking in "%s" ' % arcpy.env.workspace
     fcs = arcpy.ListFeatureClasses()
     return fcs
 
-arcpy_listFC(gdb)
+fcs = arcpy_listFC(gdb)
+print 'Feature classes found: %s' % len(fcs)
