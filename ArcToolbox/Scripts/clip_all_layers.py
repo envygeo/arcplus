65f10084e85ac9e272dae2eb8e657965a896c012
Matt.Wilkie <Matt.Wilkie@gov.yk.ca>
2015-02-26 10:04:30 -0800

    must be mapdoc object and not string for ListLayers
    See
    http://gis.stackexchange.com/questions/94890/error-with-data-frame-in-python-script

--- ArcToolbox/Scripts/clip_all_layers.py
+++ ArcToolbox/Scripts/clip_all_layers.py
@@ -1,6 +1,7 @@
 '''Clip all layers in the map to the specified polygon.
 Adapted from Alex Tereshenkov, http://gis.stackexchange.com/a/111712/108
 '''
+import os
 import arcpy
 arcpy.env.overwriteOutput = True
 
@@ -8,9 +9,12 @@ mxd = arcpy.GetParameterAsText(0)
 clip_layer = arcpy.GetParameterAsText(1)
 out_gdb = arcpy.GetParameterAsText(2)
 
-
 if not mxd:
     mxd = arcpy.mapping.MapDocument("CURRENT")
+else:
+    mxd = arcpy.mapping.MapDocument(mxd)
+        # must be mapdoc object and not string for ListLayers
+        # http://gis.stackexchange.com/questions/94890/error-with-data-frame-in-python-script
 
 for lyr in arcpy.mapping.ListLayers(mxd):
     arcpy.AddMessage(lyr)
