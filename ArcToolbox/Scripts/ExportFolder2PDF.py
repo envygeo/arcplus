3ff93bede751113a67190c4e6f52c1b2c1714948
Matt Wilkie <maphew@gmail.com>
2017-07-17 12:26:54 -0700

    standardize quoting; update docstring and comments

--- ArcToolbox/Scripts/ExportFolder2PDF.py
+++ ArcToolbox/Scripts/ExportFolder2PDF.py
@@ -1,36 +1,43 @@
-#Export a folder of maps to PDFs at their Map Document set sizes
-#Written using ArcGIS 10 and Python 2.6.5
-#by: Guest
-# https://gis.stackexchange.com/questions/7147/how-to-batch-export-mxd-to-pdf-files
+'''
+Export a folder of maps to PDFs at their Map Document set sizes
 
-import arcpy, os, glob
+Known to work ArcGIS 10.4.1 and Python 2.7.3
+Adapted from work by @bteranUFA and @Guest
+https://gis.stackexchange.com/questions/7147/how-to-batch-export-mxd-to-pdf-files
 
-#Read input parameter from user.
+License X/MIT; (c) 2017 Environment Yukon, Matt Wilkie
+'''
+
+import os
+import glob
+import arcpy
+
+# Folder to look for MXDs in
 in_path = arcpy.GetParameterAsText(0)
+# Where to save output documents
+exportPath = arcpy.GetParameterAsText(1)
 
-# Set all the parameters as variables here:
+# Set the PDF parameters as variables here:
 data_frame = 'PAGE_LAYOUT'
-df_export_width = 1920
+df_export_width = 1920  # ignored when using PAGE_LAYOUT
 df_export_height = 1200
-resolution = "300"
-image_quality = "BETTER"
-colorspace = "RGB"
-compress_vectors = "True"
-image_compression = "ADAPTIVE"
+resolution = '300'
+image_quality = 'BETTER'
+colorspace = 'RGB'
+compress_vectors = 'True'
+image_compression = 'ADAPTIVE'
 picture_symbol = 'VECTORIZE_BITMAP'
-convert_markers = "False"
-embed_fonts = "True"
-layers_attributes = "LAYERS_ONLY"
-georef_info = "False"
+convert_markers = 'False'
+embed_fonts = 'True'
+layers_attributes = 'LAYERS_ONLY'
+georef_info = 'False'
 jpeg_compression_quality = 85
-
-exportPath =arcpy.GetParameterAsText(1)
 
 maps = glob.glob(os.path.join(in_path, '*.mxd'))
 
 for m in maps:
-    dir, fname = os.path.split(m)                        # 'X:\path\to', 'some_map.mxd'
-    basename = os.path.splitext(fname)[0]                # 'some_map'
+    fname = os.path.split(m)[1]                          # ''some_map.mxd' - discard prefix path elements
+    basename = os.path.splitext(fname)[0]                # 'some_map' - discard extension
     newPDF = os.path.join(exportPath, basename + '.pdf') # 'Y:\output\some_map.pdf'
 
     arcpy.AddMessage('Reading: ' + m)
