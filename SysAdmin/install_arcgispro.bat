75f0bc865ace7f89931c98a3469ed89c778f2569
Matt Wilkie <matt.wilkie@gov.yk.ca>
2018-05-14 09:22:07 -0700

    new version from upstream original https://community.esri.com/docs/DOC-8930-arcgis-pro-silent-install-script

--- SysAdmin/install_arcgispro.bat
+++ SysAdmin/install_arcgispro.bat
@@ -1,4 +1,3 @@
-REM upstream source: https://community.esri.com/docs/DOC-8930-arcgis-pro-silent-install-script
 :: install_arcgispro.bat
 :: Silent install of ArcGIS Pro 
 :: 
@@ -22,11 +21,6 @@ REM upstream source: https://community.esri.com/docs/DOC-8930-arcgis-pro-silent-
 ::          ArcGISProHelp.cab
 ::          ArcGISProHelp.msi      
 ::
-:: References:
-:: * ArcGIS 10.4 / Pro 1.2 Enterprise Deployment
-::    http://support.esri.com/white-paper/3522
-:: * ArcGIS Pro installation guide: 
-::   .\Esri\ArcGISPro\Documentation\install.htm
 ::
 :: Author: Curtis Price, cprice@usgs.gov
 ::
@@ -35,6 +29,7 @@ REM upstream source: https://community.esri.com/docs/DOC-8930-arcgis-pro-silent-
 :: 08/19/2015 Added .NET 4.5 check
 :: 11/02/2016 Update for Pro 1.3 / 1.3.1
 :: 06/07/2017 Setup to apply any .msp files in Pro .msi folder
+:: 03/18/2018 Added CHECKFORUPDATESATSTARTUP to install command
 ::----------------------------------------------------------------
 :: This software is provisional and subject to revision; it has
 :: not been thoroughly reviewed or received USGS final approval.
@@ -44,11 +39,16 @@ REM upstream source: https://community.esri.com/docs/DOC-8930-arcgis-pro-silent-
 :: involves substantial monetary or operational consequences.
 @echo off
 
+:: keep variables local to this script
+setlocal
 
 :: %~d0%~p0 is folder where this script resides
 set SRC=%~d0%~p0
 set LOGDIR=%TEMP%
-set INSTALLDIR=C:\ArcGIS\Pro
+set INSTALLDIR=C:\Program Files\ArcGIS\Pro
+:: set to 1 to suppress Pro prompting software updates 
+:: (useful for managed deployments)
+set CHECKUPDATES=0
 
 :: ====================================================================
 ::                PLEASE DO NOT EDIT BELOW THIS LINE
@@ -56,7 +56,6 @@ set INSTALLDIR=C:\ArcGIS\Pro
 
 :: Check for .NET 4.6.1 x64
 set x64key=HKLM\SOFTWARE\Microsoft\.NETFramework
-if not defined ProgramFiles(x86) goto win32
 :: 4.6.1 x64
 %WINDIR%\system32\reg.exe query %x64key%  /f ".NETFramework,Version=v4.6.1" /k /s >NUL ^
   || ( echo. & echo ** Microsoft .NET Framework 4.6.1 ^(x64^) not installed! ** & goto dotnet_fail)  
@@ -94,18 +93,17 @@ set SILENT=/qb- /passive /norestart
 @echo on
 
 :: Install ArcGIS Pro for all users of this computer
-msiexec /i "%SRC%\Esri\ArcGISPro\ArcGISPro.msi"  ALLUSERS=1 INSTALLDIR=%INSTALLDIR% ^
-  /l+ie "%LOG_PATH%_Pro.txt" /l+ie "%LOG_PATH%.txt"  %SILENT%
+msiexec /i "%SRC%\Esri\ArcGISPro\ArcGISPro.msi" ^
+ ALLUSERS=1 INSTALLDIR="%INSTALLDIR%" ^
+ CHECKFORUPDATESATSTARTUP=%CHECKUPDATES% ^
+ /l+ie "%LOG_PATH%_Pro.txt" /l+ie "%LOG_PATH%.txt"  %SILENT%
 
 :: patch updates (.msp files (if any) should be placed in folder with ArcGIS Pro msi
 pushd "%SRC%\Esri\ArcGISPro"
 for %%p in (*.msp) do %MSIEXEC% /p %%p REINSTALLMODE=omus %SL% 
 popd
 
-:: ArcGIS Pro Help (optional local install [web help is also available])
+:: ArcGIS Pro Help (optional) (Delete or comment out to only use web help)
 msiexec /i "%SRC%\Esri\ArcGISProHelp\ArcGISProHelp.msi" ^
   /l+ie "%LOG_PATH%_Help.txt" %SILENT%
-
-:: Optional: Set registry key to block ArcGIS Pro update prompts
-%WINDIR%\system32\reg.exe add HKLM\SOFTWARE\ESRI\ArcGISPro\Settings ^
-  /v CheckForUpdatesAtStartup /t REG_DWORD /d 0 /f 
+  
