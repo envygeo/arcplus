a64199648fee239c46d7091a2e75937e1de409ed
Matt Wilkie <matt.wilkie@gov.yk.ca>
2019-06-06 15:47:29 -0700

    usage msg; xls and overwrite param; descrip bugfix
    + usage message when no parameters
    + new write XLS and Overwrite flags
    
    * bugfix: 'description' is too many characters (11),
    	kudos @dgj32784 on GIS Stack, https://gis.stackexchange.com/a/243359/108

--- ArcToolbox/Scripts/export_gdb_domains.py
+++ ArcToolbox/Scripts/export_gdb_domains.py
@@ -12,13 +12,30 @@ import os, sys
 import arcpy
 
 gdb = arcpy.GetParameterAsText(0)
+xls = str.title(arcpy.GetParameterAsText(1))
+    # True or False
+arcpy.env.overwriteOutput = str.title(arcpy.GetParameterAsText(2))
+    # True or False
+
+if not os.path.exists(gdb):
+    print('Usage:\n\n   python export_gdb_domains.py [input geodatabase] {write_xls} {overwrite}')
+    print('   python export_gdb_domains.py [input geodatabase] true false')
+    sys.exit()
+
 
 def export_domains(domains):
     for domain in domains:
         arcpy.AddMessage('Exporting %s CV to table in %s' % (domain.name, gdb))
         table = os.path.join(gdb, domain.name)
         arcpy.DomainToTable_management(gdb, domain.name, table,
-            'field','description', '#')
+            'field','descript', '#')
+        if xls:
+            os.chdir(gdb)
+            os.chdir('..')
+            xlsfile = '%s_%s.xls' % (os.path.join(os.path.basename(gdb)),domain.name)
+            arcpy.AddMessage('Exporting %s CV to table in %s' % (domain.name, xlsfile))
+            arcpy.TableToExcel_conversion(table, xlsfile)
+
 
 if __name__ == "__main__":
     domains = arcpy.da.ListDomains(gdb)
