e41850930bc88ae19de38d70251930f85317dc78
Matt Wilkie <matt.wilkie@gov.yk.ca>
2019-06-11 11:24:35 -0700

    report Tables as well as Feature Classes

--- ArcToolbox/Scripts/get-domains-in-use.py
+++ ArcToolbox/Scripts/get-domains-in-use.py
@@ -1,6 +1,6 @@
-'''List feature classes and sub-types using attribute domains within gdb
+'''List feature classes, sub-types and tables using attribute domains within gdb
 
-Courtesy of Richard Morgan - https://gis.stackexchange.com/a/217992/108
+Adapted from Richard Morgan - https://gis.stackexchange.com/a/217992/108
 '''
 import arcpy
 
@@ -11,23 +11,36 @@ if not gdb:
 
 arcpy.env.workspace = gdb
 
+header = "{},{},{},{}".format('Item', 'Subtype', 'Field', 'Domain')
 def main(gdb):
-    header = "{},{},{},{}".format('Feature_class', 'Subtype', 'Field', 'Domain')
+    '''Returns nested lists of Feature Class or Table, then active domains
+    within the FC/Table'''
     result = []
     for FC in arcpy.ListFeatureClasses():
-        for stcode, stdict in list(arcpy.da.ListSubtypes(FC).items()):
-            for stkey in list(stdict.keys()):
-                if stkey == 'FieldValues':
-                    for field, fieldvals in list(stdict[stkey].items()):
-                        if fieldvals[1] is not None:
-                            result.append("{},{},{},{}".format(
-                                FC,
-                                'None' if stcode == 0 else stdict['Name'],
-                                field,
-                                fieldvals[1].name))
+        result.append(find_in_use_domains(FC))
+    for TB in arcpy.ListTables():
+        result.append(find_in_use_domains(TB))
     return result
+
+def find_in_use_domains(item):
+    '''Return field names with active domains in the feature class or table'''
+    result = []
+    for stcode, stdict in list(arcpy.da.ListSubtypes(item).items()):
+        for stkey in list(stdict.keys()):
+            if stkey == 'FieldValues':
+                for field, fieldvals in list(stdict[stkey].items()):
+                    if fieldvals[1] is not None:
+                        result.append("{},{},{},{}".format(
+                            item,
+                            'None' if stcode == 0 else stdict['Name'],
+                            field,
+                            fieldvals[1].name))
+    return result
+
 
 if __name__ == '__main__':
     rows = main(gdb)
     print(header)
-    [print(x) for x in rows]
+    for r in rows:
+        if r:
+            [print(x) for x in r]
