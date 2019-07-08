63d5f5c0d2c55f869cdc2313b161d7a9c40567d9
Matt Wilkie <matt.wilkie@gov.yk.ca>
2019-07-08 14:28:16 -0700

    new: command line args for Start and End codes
    > Usage: gpcodes [start number] {optional end number}
    
    Returns only the start number of end is omitted.
    
    Also modified output to tab instead of delimited for better parsing later.

--- arcplus/gpcodes.py
+++ arcplus/gpcodes.py
@@ -1,17 +1,29 @@
 ''' Generate list of possible geoprocessing error codes. These error codes are
    not documented in the help anymore so Curtis wrote a script to make a list.
-   Curtis Price <cprice@usgs.gov>, 2019-Jul-05
+   Initial version: 2019-Jul-05, Curtis Price <cprice@usgs.gov>
 '''
 import sys
 import arcpy
+
+# start code to fetch
 try:
   n = int(sys.argv[1])
-except:
-  n = 20
-for k in range(n):
+except IndexError:
+  print('''Usage: gpcodes [start number] {optional end number}\n''')
+  sys.exit()
+
+# optional end code to fetch
+try:
+  nn = int(sys.argv[2])
+except IndexError:
+  nn = n # default to start number
+
+nn += 1 # increment by 1 so we return the final code as well
+
+for k in range(n,nn):
   try:
     dsc = arcpy.GetIDMessage(k)
     if dsc:
-        print("{:6d} {}".format(k, dsc))
+        print("{:6d}\t{}".format(k, dsc))
   except:
     pass
