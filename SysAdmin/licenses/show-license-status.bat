ed5b849eb7a89a8b7317f8bf7fd71ece11441afe
Matt Wilkie <Matt.Wilkie@yukon.ca>
2022-03-14 11:54:28 -0700

    Show Esri license status bat file

---
+++ SysAdmin/licenses/show-license-status.bat
@@ -0,0 +1,17 @@
+@echo off
+:: Report  ESRI License Manager  status
+:: 2009.August.05 *  matt.wilikie@gov.yk.ca * this script is public domain.
+setlocal
+path=%path%;%~dp0\bin
+
+:: use hardcoded server unless  server/ip specified on command line
+if [%1]==[] (
+      set arclic_server=ENVGEOSERVER
+   ) else (
+      set arclic_server=%1
+      )
+
+lmutil lmstat -a -c @%arclic_server%
+
+pause
+
