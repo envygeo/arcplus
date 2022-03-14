cb0586ca40ca319e0d57c02c16c652d0dc2e2833
Matt Wilkie <Matt.Wilkie@yukon.ca>
2022-03-14 11:59:52 -0700

    add comment re: dependencies

--- SysAdmin/licenses/show-license-status.bat
+++ SysAdmin/licenses/show-license-status.bat
@@ -1,6 +1,8 @@
 @echo off
 :: Report  ESRI License Manager  status
 :: 2009.August.05 *  matt.wilikie@gov.yk.ca * this script is public domain.
+:: Requires Lmutil.exe and Lmgrd.exe in PATH.  Find them under license 
+:: manager in C:\Program Files. They can be copied and run from anywhere.
 setlocal
 path=%path%;%~dp0\bin
 
@@ -14,4 +16,3 @@ if [%1]==[] (
 lmutil lmstat -a -c @%arclic_server%
 
 pause
-
