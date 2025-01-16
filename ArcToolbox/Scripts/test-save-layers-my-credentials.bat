917000047db070622d287e85194ccecc9f882b2a
Matt Wilkie <matt.wilkie@yukon.ca>
2025-01-16 09:02:38 -0700

    test runner

---
+++ ArcToolbox/Scripts/test-save-layers-my-credentials.bat
@@ -0,0 +1,4 @@
+python save-layers-my-credentials.py ^
+  "\\portal-prd\ProLayerfiles\Administrative Boundaries\Yukon Border" ^
+  "z:\Common\Connections\Connection to cswprod.sde" ^
+  d:\csw-layers
